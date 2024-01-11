72d5257cd8172c6c73ae8fda744a5ccf1ddf3be0
Enr Labs <dev@engramnet.io>
2024-01-11 05:01:57 +0000

    main

---
+++ run.sh
@@ -0,0 +1,7 @@
+yarn run start \
+  --private-key <PK_ADDRESS> \
+  --json-rpc https://tokioswift.engram.tech/ \
+  --weth9-address 0x7E3e3C50927F78ce0D2a1699d15d342c976A49B0 \
+  --native-currency-label tGRAM \
+  --owner-address <OWNER_PK_ADDRESS> \
+  --confirmations 2
